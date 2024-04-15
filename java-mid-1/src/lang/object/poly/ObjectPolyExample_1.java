b8f56add419a12dc1d6f36e7c3ad4df6de187f24
yXoXo9 <ygt1009@gmail.com>
2024-04-15 23:02:54 +0900

    1_Object_Poly_1

---
+++ java-mid-1/src/lang/object/poly/ObjectPolyExample_1.java
@@ -0,0 +1,31 @@
+package lang.object.poly;
+
+public class ObjectPolyExample_1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+    }
+
+    private static void action (Object obj){
+        /*
+        부모 = 자식 가능
+        Dog dog2 = new Dog();
+        Object objx = dog2;
+        */
+        // Object objx = new Dog(); // 가능
+
+        // obj 는 들어온 객체가 무엇인지 모름
+        // obj.sound(); 컴파일 오류 발생
+        // obj.move(); 컴파일 오류 발생
+
+        // => 객체에 맞는 다운 캐스팅 필요
+        if(obj instanceof Dog getDog){
+            getDog.sound();
+        }else if(obj instanceof Car getCar){
+            getCar.move();
+        }
+    }
+}
