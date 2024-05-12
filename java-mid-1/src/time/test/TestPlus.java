f4bf0485d62257de65a6a1254c82c23c3345cfce
yXoXo9 <ygt1009@gmail.com>
2024-05-12 23:30:07 +0900

    [섹션_6]_Date&Time_p.6...T.T

--- java-mid-1/src/time/test/TestPlus.java
+++ java-mid-1/src/time/test/TestPlus.java
@@ -1,11 +1,6 @@
 package time.test;
 
-import java.text.DateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.ChronoPeriod;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.*;
 
@@ -17,6 +12,51 @@ public class TestPlus {
         quest_2();
         System.out.println();
 
+        quest_3();
+        System.out.println();
+
+        quest_4(); // T.T
+        System.out.println();
+
+        quest_5(); // T.T
+        System.out.println();
+
+
+    }
+
+    private static void quest_5() { // T.T
+        /*
+        * 서울의 회의 시간은 2024년 1월 1일 오전 9시, 이때 런던, 뉴욕의 회의 시간을 구할 것
+        * */
+        LocalDateTime ldt = LocalDateTime.of(2024, 1, 1, 9, 00, 00);
+        System.out.println("ldt = " + ldt);
+        System.out.println("서울의 회의 시간: " + ZonedDateTime.of(ldt, ZoneId.of("Asia/Seoul")));
+        System.out.println("런던의 회의 시간 : " + ZonedDateTime.of(ldt, ZoneId.of("Europe/London")));
+        System.out.println("뉴욕의 회의 시간 : " + ZonedDateTime.of(ldt, ZoneId.of("America/New_York")));
+
+        ZonedDateTime seoulZoneId = ZonedDateTime.of(LocalDate.of(2024, 1, 1), LocalTime.of(9, 0), ZoneId.of(("Asia/Seoul")));
+        System.out.println("\nseoulZoneId = " + seoulZoneId);
+        ZonedDateTime londonZoneId = seoulZoneId.withZoneSameInstant(ZoneId.of("Europe/London"));
+        System.out.println("londonZoneId = " + londonZoneId);
+        ZonedDateTime newYorkZoneId = seoulZoneId.withZoneSameInstant(ZoneId.of("America/New_York"));
+        System.out.println("newYorkZoneId = " + newYorkZoneId);
+    }
+
+    private static void quest_4() { // T.T
+        /*
+        * 입력 받은 월의 첫날 요일과 마지막날 요일을 구할것.....
+        * */
+        int year = 2024;
+        int month = 1;
+
+        LocalDate inputLdt = LocalDate.of(year, month, 1);
+        System.out.println("firstDayOfWeek : " + inputLdt.getDayOfWeek());
+        DayOfWeek dayOfWeek = inputLdt.with(TemporalAdjusters.lastDayOfMonth()).getDayOfWeek();
+        System.out.println("LastDayOfWeek = " + dayOfWeek);
+        // TemporalAdjuster temporalAdjuster = TemporalAdjusters.firstInMonth(inputLdt.getDayOfWeek());
+    }
+
+    private static void quest_3() {
         /*
         * 시작 날짜와 목표 날짜를 입력하여 남은 기간과 디데이를 구할 것
         * 남은 기간: x년 x개월 x일 형식으로 출력
@@ -32,9 +72,14 @@ public class TestPlus {
 
         LocalDate diffDate = endDate.minusYears(startDate.getYear()).minusMonths(startDate.getMonthValue()).minusDays(startDate.getDayOfMonth());
         System.out.println("2.남은 기간: " + diffDate.format(DateTimeFormatter.ofPattern("Y년 MM개월 dd일")));
+        int year = endDate.minusYears(startDate.getYear()).getYear();
+        System.out.println("localDate1 = " + year);
 
         LocalDate localDate = endDate.minusDays(startDate.getDayOfYear());
-        System.out.println("디데이: " + localDate.getDayOfYear() + "일 남은");
+        System.out.println("1_디데이: " + localDate.getDayOfYear() + "일 남은");
+
+        long between = ChronoUnit.DAYS.between(startDate, endDate);
+        System.out.println("2.디데이: " + between);
     }
 
     private static void quest_2() {
