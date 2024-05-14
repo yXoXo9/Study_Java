f0bab60c848e4ce0d48d7eed62592e367e69ff1e
yXoXo9 <ygt1009@gmail.com>
2024-05-14 23:31:36 +0900

    [섹션_6]_Date&Time_p.8...T.T

--- java-mid-1/src/time/test/TestPlus_2.java
+++ java-mid-1/src/time/test/TestPlus_2.java
@@ -11,30 +11,41 @@ import java.util.Scanner;
 
 public class TestPlus_2 {
     public static void main(String[] args) {
+        // diy();
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("년도 입력 : ");
+        int iYear = scanner.nextInt();
+        System.out.print("월 입력 : ");
+        int iMonth= scanner.nextInt();
+
+        printCalender(iYear, iMonth);
+
+    }
+
+    private static void printCalender(int iYear, int iMonth) {
+        
+    }
+
+    private static void diy() {
         Scanner sc = new Scanner(System.in);
         System.out.print("년도를 입력하세요: ");
-//        int getYear = sc.nextInt();
+        int getYear = sc.nextInt();
         System.out.print("월을 입력하세요: ");
-        System.out.println();
-//        int getMonth = sc.nextInt();
+        int getMonth = sc.nextInt();
 
-//        System.out.println("getYear = " + getYear);
-//        System.out.println("getMonth = " + getMonth);
-
-
-        LocalDate localDate = LocalDate.of(2025, 1, 1);
-
-        System.out.println("\nlocalDate.getDayOfWeek() = " + localDate.getDayOfWeek());
+        LocalDate localDate = LocalDate.of(getYear, getMonth, 1);
+        System.out.println("\nlocalDate.getDayOfWeek() = " + localDate.getDayOfWeek().getValue());
         int dayOfMonth = localDate.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
-        System.out.println("\ndayOfMonth = " + dayOfMonth);
-        System.out.println();
-        System.out.println("SU\tMo\tTu\tWe\tTh\tFr\tSa");
-        int cnt = 1;
-        for (int i = 1, j = 1; i <= dayOfMonth+3; i++) {
+        System.out.println("dayOfMonth = " + dayOfMonth);
+
+        System.out.println(getYear + " / " + getMonth + "\n");
+        System.out.println("Su\tMo\tTu\tWe\tTh\tFr\tSa");
+        // System.out.println("알\t월\t화\t수\t목\t금\t토");
+        int interval = localDate.getDayOfWeek().getValue();
+        for (int i = 1, j = 1; i <= dayOfMonth + interval; i++) {
             String align = (i % 7 != 0) ? "\t" : "\n";
-            System.out.print(((i < 13) ? " " : "") + ((i == (j+3)) ? j++ : "") + align);
+            System.out.print(((i < 10 + interval) ? " " : "") + ((i == (j + interval)) ? j++ : "") + align);
         }
-
-
     }
 }
