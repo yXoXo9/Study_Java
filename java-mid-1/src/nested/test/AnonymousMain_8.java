6976d30c939642dc95f142fdafb7a279f3625716
yXoXo9 <ygt1009@gmail.com>
2024-05-24 23:09:54 +0900

    [섹션_8]_Nested_Class&InnerClass_Cap2_p.6...T.T

--- java-mid-1/src/nested/test/AnonymousMain_8.java
+++ java-mid-1/src/nested/test/AnonymousMain_8.java
@@ -2,7 +2,7 @@ package nested.test;
 
 public class AnonymousMain_8 {
     public static void main(String[] args) {
-        Hello_7 hello7 = () -> System.out.println("Hello_7.hello");
+        Hello_7 hello7 = () -> System.out.println("Hello7.hello");
         hello7.hello();
     }
 }
