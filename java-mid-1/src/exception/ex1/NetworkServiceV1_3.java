1ec134898fdef2e041220e454e139ecf133d0c55
yXoXo9 <ygt1009@gmail.com>
2024-05-28 23:39:45 +0900

    [섹션_9]_Exception_proc_Cap1_p.2...T.T

---
+++ java-mid-1/src/exception/ex1/NetworkServiceV1_3.java
@@ -0,0 +1,33 @@
+package exception.ex1;
+
+public class NetworkServiceV1_3 {
+    public void sendMessage(String data){
+        String accessAddress = "http://example.com";
+        NetworkClientV1_0 client = new NetworkClientV1_0(accessAddress);
+
+        client.initError(data);
+
+        // 메시지 연결하기 위해서는 서버 연결 먼저.
+        String connectResult = client.connect();
+
+        // 결과가 성공이 아니다 => 오류다.
+        // => 조건을 사람이 이해하기 더 쉽게 리팩토링
+        if(isError(connectResult)){
+            System.out.println("네트워크 오류 발생 코드 : " + connectResult);
+            return;
+        }
+
+        String sendResult = client.send(data);
+        if (isError(sendResult)){
+            System.out.println("네트워크 전송 발생 코드 : " + sendResult);
+            return;
+        }
+
+        client.disconnect();
+
+    }
+
+    private static boolean isError(String connectResult) {
+        return !connectResult.equals("Success");
+    }
+}
