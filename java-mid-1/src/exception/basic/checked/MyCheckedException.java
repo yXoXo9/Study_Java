2aa674a3b4622889fc24ffcc51d766b4a94d5030
yXoXo9 <ygt1009@gmail.com>
2024-06-01 23:40:15 +0900

    T

--- java-mid-1/src/exception/basic/checked/MyCheckedException.java
+++ java-mid-1/src/exception/basic/checked/MyCheckedException.java
@@ -1,7 +1,6 @@
 package exception.basic.checked;
 
-/*
-* Exception 을 상속받은 예외는 체크 예외가 된다 => 상속 받으면 전부 체크 예외
+/* Exception 을 상속받은 예외는 체크 예외가 된다 => 상속 받으면 전부 체크 예외
 * 예외 클래스를 만들려면 예외를 상속 받으면 가능하다.
 * */
 public class MyCheckedException extends Exception {
