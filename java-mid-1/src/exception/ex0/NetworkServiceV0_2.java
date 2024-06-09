84699a72daf834a16e11ea4d1c408a589888cf40
yXoXo9 <ygt1009@gmail.com>
2024-06-09 21:20:41 +0900

    T

--- java-mid-1/src/exception/ex0/NetworkServiceV0_2.java
+++ java-mid-1/src/exception/ex0/NetworkServiceV0_2.java
@@ -7,10 +7,8 @@ public class NetworkServiceV0_2 {
 
         // 메시지 연결하기 위해서는 서버 연결 먼저.
         client.connect();
-
         // 메시지 전송
         client.send(data);
-
         // 네트워크 연결 해제
         client.disconnect();
 
