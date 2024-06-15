59930cc2d5e041b5cb4b854d95aafde6fe7cc2b3
yXoXo9 <ygt1009@gmail.com>
2024-06-15 23:32:40 +0900

    T

--- java-mid-1/src/enumeration/ex3/Grade.java
+++ java-mid-1/src/enumeration/ex3/Grade.java
@@ -12,7 +12,6 @@ public enum Grade {
 
     /*
     하기 코드와 동일한 기능을 수행 (enum 또한 클래스다)
-
     열거형은 자동으로 java.lang.Enum 을 상속받는다.
     외부에서 임의로 생성할 수 없다.
 
@@ -20,7 +19,6 @@ public enum Grade {
         public static final Grade BASIC = new Grade();
         public static final Grade GOLD = new Grade();
         public static final Grade DIAMOND = new Grade();
-
         private Grade() {}
     }
     */
