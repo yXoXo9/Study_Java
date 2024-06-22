67fcc7bc54822eaa6d0a38f343e03af29797f4f8
yXoXo9 <ygt1009@gmail.com>
2024-06-22 23:52:00 +0900

    T

--- java-mid-1/src/exception/ex1/NetworkServiceV1_3.java
+++ java-mid-1/src/exception/ex1/NetworkServiceV1_3.java
@@ -12,15 +12,14 @@ public class NetworkServiceV1_3 {
         // 결과가 성공이 아니다 => 오류다.
         // => 조건을 사람이 이해하기 더 쉽게 리팩토링
         if(isError(connectResult)){
-            System.out.println("네트워크 오류 발생 코드 : " + connectResult);
+            System.out.println("네트워크 오류 시 발생 코드 : " + connectResult);
             return;
         }
         String sendResult = client.send(data);
         if (isError(sendResult)){
-            System.out.println("네트워크 전송 발생 코드 : " + sendResult);
+            System.out.println("네트워크 전송 시 발생 코드 : " + sendResult);
             return;
         }
-
         client.disconnect();
     }
 
