871243e280a7b316bf8fbd427320224964add3a8
yXoXo9 <ygt1009@gmail.com>
2024-07-07 23:31:26 +0900

    T

--- java-mid-1/src/exception/basic/checked/ExceptionClient_2.java
+++ java-mid-1/src/exception/basic/checked/ExceptionClient_2.java
@@ -7,9 +7,7 @@ public class ExceptionClient_2 {
     // 1. throw 는 시점에 터트리는것,
     // 2. throws 는 밖으로 던지는것
     public void call() throws MyCheckedException{ // 클래스를 호출한 곳(서비스)으로 예외를 던짐
-
         // 문제 상황 발생(throw) _ 예시 (예외 또한 객체이므로 생성 후 발생시켜야 한다)
         throw new MyCheckedException("excep");
-
     }
 }
