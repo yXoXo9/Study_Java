8e9766eca7a151443980f4612c9059518a06616e
yXoXo9 <ygt1009@gmail.com>
2024-07-21 19:47:19 +0900

    T

--- java-mid-1/src/exception/ex1/NetworkMainV1_2.java
+++ java-mid-1/src/exception/ex1/NetworkMainV1_2.java
@@ -7,9 +7,7 @@ public class NetworkMainV1_2 {
         // NetworkServiceV1_1 networkServiceV02 = new NetworkServiceV1_1();
         // NetworkServiceV1_3 networkServiceV02 = new NetworkServiceV1_3();
         NetworkServiceV1_4 networkServiceV02 = new NetworkServiceV1_4();
-
         Scanner scanner = new Scanner(System.in);
-
         while (true){
             System.out.print("메시지 입력 : ");
             String inputString = scanner.nextLine();
