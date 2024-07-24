7d0dc07322abab7da72f8a0eb0a06ad3d7e4b9d9
yXoXo9 <ygt1009@gmail.com>
2024-07-24 22:04:57 +0900

    T

--- java-mid-1/src/exception/ex1/NetworkServiceV1_4.java
+++ java-mid-1/src/exception/ex1/NetworkServiceV1_4.java
@@ -2,12 +2,12 @@ package exception.ex1;
 
 public class NetworkServiceV1_4 {
     public void sendMessage(String data){
+
         String accessAddress = "http://example.com";
         NetworkClientV1_0 client = new NetworkClientV1_0(accessAddress);
 
         client.initError(data);
         String connectResult = client.connect();
-
         if(isError(connectResult)){
             System.out.println("네트워크 오류 발생 코드 : " + connectResult);
             return;
