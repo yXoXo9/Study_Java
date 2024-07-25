9004624d30b44ae51e0ef5185a0926b4d6351b6a
yXoXo9 <ygt1009@gmail.com>
2024-07-25 22:03:37 +0900

    T

--- java-mid-1/src/exception/ex1/NetworkServiceV1_4.java
+++ java-mid-1/src/exception/ex1/NetworkServiceV1_4.java
@@ -8,6 +8,7 @@ public class NetworkServiceV1_4 {
 
         client.initError(data);
         String connectResult = client.connect();
+
         if(isError(connectResult)){
             System.out.println("네트워크 오류 발생 코드 : " + connectResult);
             return;
@@ -18,6 +19,7 @@ public class NetworkServiceV1_4 {
                 return;
             }
         }
+
         client.disconnect();
     }
 
