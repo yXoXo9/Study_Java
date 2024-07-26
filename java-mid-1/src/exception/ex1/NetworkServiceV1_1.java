94591f532b656a44439fe4558dc7a66f1f2e1a1a
yXoXo9 <ygt1009@gmail.com>
2024-07-26 22:29:17 +0900

    T

--- java-mid-1/src/exception/ex1/NetworkServiceV1_1.java
+++ java-mid-1/src/exception/ex1/NetworkServiceV1_1.java
@@ -7,13 +7,10 @@ public class NetworkServiceV1_1 {
 
         // 에러 컨트롤
         client.initError(data);
-
         // 메시지 연결하기 위해서는 서버 연결 먼저.
         client.connect();
-
         // 메시지 전송
         client.send(data);
-
         // 네트워크 연결 해제
         client.disconnect();
     }
