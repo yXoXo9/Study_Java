9629a70b93a6e3c86541d0feee9a0ee01b6b74df
yXoXo9 <ygt1009@gmail.com>
2024-07-28 20:39:18 +0900

    T

--- java-mid-1/src/exception/basic/checked/MyCheckedException.java
+++ java-mid-1/src/exception/basic/checked/MyCheckedException.java
@@ -5,7 +5,6 @@ package exception.basic.checked;
 * 예외 클래스를 만들려면 예외를 상속 받으면 가능하다.
 * */
 public class MyCheckedException extends Exception {
-
     // 오류 메시지를 받은 후 부모에 넘겨 줌
     public MyCheckedException(String message) {
         super(message);
