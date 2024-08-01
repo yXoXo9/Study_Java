d23437f1691612e3484b399fc96a236bf6cffc4b
yXoXo9 <ygt1009@gmail.com>
2024-08-01 22:51:37 +0900

    T

--- java-mid-1/src/exception/basic/checked/CheckedCatchMain.java
+++ java-mid-1/src/exception/basic/checked/CheckedCatchMain.java
@@ -2,8 +2,10 @@ package exception.basic.checked;
 
 public class CheckedCatchMain {
     public static void main(String[] args) {
+
         ExceptionService exceptionService = new ExceptionService();
         exceptionService.callCatch();
+
         System.out.println("정상종료");
     }
 }
